f0524d487872e93d6d2e2ad995ec8bd770c3b95a
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-11-14 11:19:45 +1100

    iterating through user DB successful

--- index.rb
+++ index.rb
@@ -1,3 +1,14 @@
 require 'json'
 require_relative "./database_setup.rb"
-puts $users[0]["name"]
+puts "Please enter your name"
+input = gets.chomp.downcase
+
+$users.each do |user|
+  puts user.class  
+  if user["name"] == input 
+    puts "Hello #{user["name"].upcase}"
+    break
+  else 
+    puts "Could not find #{input}"
+  end 
+end 
